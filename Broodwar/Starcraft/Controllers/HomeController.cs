c47ee3024aee76db6dc0b77e2c6ef7379d58eb08
SungHo.You <hahaysh@hotmail.com>
2019-06-26 21:53:10 +0900

    add sample project

---
+++ Broodwar/Starcraft/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Starcraft.Models;
+
+namespace Starcraft.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            //클래스 라이브러리 참조
+            StarcraftLibrary.GoDoWa goDowa = new StarcraftLibrary.GoDoWa();
+            string strTemp = string.Empty;
+            strTemp = goDowa.ReturnReturn(ref strTemp, new string[] { "저그와 ", "테란" });
+
+            Thread.Sleep(3000);
+
+            ViewBag.Label = strTemp;
+
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Broodwar/StarcraftLibrary/GoDoWa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarcraftLibrary
+{
+    public class GoDoWa
+    {
+        public string ReturnReturn(ref string strTemp, string[] x)
+        {
+            //string strTemp = string.Empty;
+
+            try
+            {
+                strTemp = x[0] + x[1];
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.ToString());
+            }
+            return strTemp;
+        }
+    }
+}
